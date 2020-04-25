2bfb28940da61f751dc3ec13afae27da8c7c54fc
Loretta Tappan <lorettatappan@gmail.com>
2020-04-25 10:29:49 -0400

    Update ElevenNote 10 Migrations

---
+++ ElevenNote.Models/NoteDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+
+namespace ElevenNote.Models
+{
+    public class NoteDetail
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
